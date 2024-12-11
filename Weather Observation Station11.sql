6950fb7fea4c6716084b63d9720c18d7dff981ce
Belal30311 <011022bn@gmail.com>
2024-12-11 17:09:01 +0200

    city don't start with [aeiou] or don't end with [aeiou]

---
+++ Weather Observation Station11.sql
@@ -0,0 +1,21 @@
+select distinct city
+from station
+where 
+     (
+    -- start with [aeiou]
+         lower(city) not like 'a%'
+         and lower(city) not  like 'e%'
+        and lower(city) not like 'i%'
+        and lower(city) not like 'o%'
+        and lower(city) not like 'u%'
+         )
+         or
+         (
+    -- end with [aeiou]
+        lower(city) not like '%a'
+        and lower(city) not  like '%e'
+        and lower(city) not like'%i' 
+        and lower(city) not like '%o' 
+        and lower(city) not like '%u' 
+         )
+        ;
